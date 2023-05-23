df889825384aa44815573b3bc0c15f7a2fc95af9
kisio <derrickkisio@gmail.com>
2023-05-23 08:53:32 +0300

    API Authorization with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\MovieController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/movies',MovieController::class);
+Route::apiResource('/movies',MovieController::class)->middleware('auth:sanctum');
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email'=>'required',
+        'password'=>'required'
+    ]);
+    $user= User::whereEmail($request->email)->first();
+    if($user){
+        $token = $user->createToken('movies');
+    }
+
+    return ['token' => $token->plainTextToken];
+});
